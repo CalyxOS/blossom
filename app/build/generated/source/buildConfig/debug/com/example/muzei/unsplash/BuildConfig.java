0678938646b60f6a409068e66faad0a7f8fc12db
Chirayu Desai <chirayudesai1@gmail.com>
2021-01-19 01:10:36 +0530

    [DO NOT MERGE] Make it build
    * Will be split up into proper commits
    
    Change-Id: I479387dbe127123fb9bf42ef542d474423132a5c

---
+++ app/build/generated/source/buildConfig/debug/com/example/muzei/unsplash/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.muzei.unsplash;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.muzei.unsplash";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String UNSPLASH_AUTHORITY = "com.example.muzei.exampleunsplash";
+}
---
+++ app/build/generated/source/buildConfig/release/com/example/muzei/unsplash/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.muzei.unsplash;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.muzei.unsplash";
+  public static final String BUILD_TYPE = "release";
+  // Field from default config.
+  public static final String UNSPLASH_AUTHORITY = "com.example.muzei.exampleunsplash";
+}
